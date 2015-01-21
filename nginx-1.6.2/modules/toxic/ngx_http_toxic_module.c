2896e3075610acbbdb990c5c87d4bb37c7794298
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-01-22 00:14:30 +0400

    again finilize

--- nginx-1.6.2/modules/toxic/ngx_http_toxic_module.c
+++ nginx-1.6.2/modules/toxic/ngx_http_toxic_module.c
@@ -286,11 +286,6 @@ static void toxic_post_body_handler(ngx_http_request_t *r)
     {
         toxic_parse_post(r);
         toxic_excecute(r);
-        while(1)
-        {
-            if(r->connection->close)
-                exit(1);
-        }
     }
     ngx_http_finalize_request(r, NGX_OK);
 }
