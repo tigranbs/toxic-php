39712ba5e1b2aa2b461e69748fd417036350f79f
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-01-25 02:02:53 +0400

    again finilize

--- nginx-1.6.2/modules/toxic/ngx_http_toxic_module.c
+++ nginx-1.6.2/modules/toxic/ngx_http_toxic_module.c
@@ -283,7 +283,7 @@ static void end_event(ngx_event_t *ev)
 {
     ngx_connection_t *c;
     c = (ngx_connection_t *)ev->data;
-    if (c->close)
+    if (c->write->complete)
     {
         exit(1);
     }
